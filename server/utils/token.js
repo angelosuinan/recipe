e94389efdadcd252c68355a8ddb5e6c2a14683a9
Angelo Suinan <suinanangelo@gmail.com>
2019-11-09 12:38:19 +0800

    :bug: Catch no auth token

--- server/utils/token.js
+++ server/utils/token.js
@@ -1,6 +1,7 @@
 module.exports = {
   getUserFromContext (context) {
     let token = context.headers['authorization'] // Express headers are auto converted to lowercase
+    if (!token) throw new Error('Unauthorized access')
     token = token.slice(7, token.length)
     return token
   },
